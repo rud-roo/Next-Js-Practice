6b4ceff440cebe3b8c89f17b9d3442ed747dec30
Rudro Saha <saharudro15@gmail.com>
2024-11-20 16:40:25 +0600

    learn about static paths, isr (time based and on-demand) in page router

--- src/pages/posts/posts.jsx
+++ src/pages/posts/posts.jsx
@@ -1,10 +1,15 @@
+import { revalidatePath } from "next/cache";
+import Link from "next/link";
+
 const Posts = ({ posts }) => {
 	return (
 		<div>
 			<h2>All Posts</h2>
 			<ul>
 				{posts.map((post) => (
-					<li key={post.id}>{post.title}</li>
+					<li key={post.id}>
+						<Link href={`/posts/${post.id}`}>{post.title}</Link>
+					</li>
 				))}
 			</ul>
 		</div>
@@ -14,10 +19,10 @@ const Posts = ({ posts }) => {
 export async function getStaticProps() {
 	console.log("GetStaticProps from Posts page");
 	const res = await fetch(
-		"https://jsonplaceholder.typicode.com/posts?_limit=5",
+		"http://localhost:4000/posts",
 	);
 	const posts = await res.json();
-	return { props: { posts } };
+	return { props: { posts }, revalidate: 20 };
 }
 
 export default Posts;
