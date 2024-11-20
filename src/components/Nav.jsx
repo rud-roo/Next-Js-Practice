60e82a602af18daa70b06639912a0360de309e6b
Rudro Saha <saharudro15@gmail.com>
2024-11-20 10:34:49 +0600

    Learn about the fundamentals of next js using page router

---
+++ src/components/Nav.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+function Nav() {
+	return (
+		<header className="header">
+			<div className="container">
+				<nav className="header__navbar">
+					<ul>
+						<li>
+							<Link href="/">Shop</Link>
+						</li>
+						<li>
+							<Link href="/about">About</Link>
+						</li>
+						<li>
+							<Link href="/posts/posts">PostList</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	);
+}
+
+export default Nav;
